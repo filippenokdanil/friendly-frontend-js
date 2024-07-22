624d3261b6f2dc73547a5300f09e3bf2977efce5
filippenokdanil <danil-filippenok@mail.ru>
2024-07-22 13:54:36 +0500

    Объеденения объектов

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 "use strict";
 
-const user = { 
-  name: 'Данил',
-  age: 22,
-  isDeveloper: true,
-  address: {
-    city: 'Курган',
-    zipcode: '123456',
-  }, 
-};
+const objName = { name: 'Данил' };
+const objAge = { age: 28 };
 
+const user = {};
 console.log( user );
-console.log( user.address?.city );
 
-console.log('Hello, World!');
+// Объединение объектов первым способом
+Object.assign(user, objName, objAge);
+console.log( "user: ", user );
+
+// Объединение объектов вторым способом
+const userOther = Object.assign({}, objName, objAge);
+console.log( "userOther:", userOther );
+
+// Объединение объектов третьим способом
+const userOtherMore = { ...objName, ...objAge };
+console.log( "userOtherMore:", userOtherMore );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
